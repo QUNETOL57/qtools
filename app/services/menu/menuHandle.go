7e241b1a2940f62f7a7ad4b1920c48e5077fb55a
qunetol <qunetol57@outlook.com>
2023-08-14 22:49:55 +0300

    3

--- app/services/menu/menuHandle.go
+++ app/services/menu/menuHandle.go
@@ -1,6 +1,7 @@
 package menu
 
 import (
+	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
@@ -13,42 +14,31 @@ const (
 	StateTarrayMenu
 )
 
-
 type Menu struct {
-    Items     map[BotState]MenuItem // Карта элементов меню, ключ - идентификатор элемента
+	Items map[BotState]MenuItem // Карта элементов меню, ключ - идентификатор элемента
 }
 
-type MenuItem struct {
-    Text         string            // Текст элемента меню
-    NextMenuId   string            // Идентификатор следующего подменю
-    Command      func()            // Функция-обработчик команды, связанной с элементом меню
-	Handle 		 func()
-	SubItems	 map[BotState]MenuItem
-
-}
-
-func (m *Menu) GetUserState(userId string) string {
-    return m.UserState[userId]
-}
+//func (m *Menu) GetUserState(userId string) string {
+//    return m.UserState[userId]
+//}
 
 func (m *Menu) HandleCommand(userId string, command string) {
-    currentItem := m.Items[m.GetUserState(userId)] // Получаем текущий элемент меню
+	currentItem := m.Items[m.GetUserState(userId)] // Получаем текущий элемент меню
 
-    if currentItem.Command != nil {
-        currentItem.Command() // Выполняем функцию-обработчик команды текущего элемента меню
-    }
+	if currentItem.Command != nil {
+		currentItem.Command() // Выполняем функцию-обработчик команды текущего элемента меню
+	}
 
-    nextMenuId := currentItem.NextMenuId // Получаем идентификатор следующего подменю
+	nextMenuId := currentItem.NextMenuId // Получаем идентификатор следующего подменю
 
-    if nextMenuId != "" {
-        m.SetUserState(userId, nextMenuId) // Устанавливаем состояние пользователя как идентификатор следующего подменю
-    }
+	if nextMenuId != "" {
+		m.SetUserState(userId, nextMenuId) // Устанавливаем состояние пользователя как идентификатор следующего подменю
+	}
 }
 
-
-func (m *Menu) SetUserState(userId string, state string) {
-    usersStates[userId] = []
-}
+//func (m *Menu) SetUserState(userId string, state string) {
+//    usersStates[userId] = []
+//}
 
 func (m *Menu) GetMenuState(userId) {
 	states := usersStates[userId]
@@ -58,110 +48,109 @@ func (m *Menu) GetMenuState(userId) {
 		for _, state := range states[1:] {
 			&menuState.SubItems[state]
 		}
-		
+
 	}
 
 	return menuState
 }
 
+type MenuItem struct {
+	Text       string   // Текст элемента меню
+	NextMenuId BotState // Идентификатор следующего подменю
+	Command    func()   // Функция-обработчик команды, связанной с элементом меню
+	Handle     func()
+	SubItems   map[BotState]MenuItem
+}
+
 func main() {
-    menu := Menu{
-        Items:     make(map[BotState]MenuItem){
-			StateMainMenu : MenuItem{
-				Text: "Main menu",
-				Command: func() { fmt.Println("Вывод главного меню") }
-				Handle: func() { fmt.Println("Обработчик для меню") }
+	menu := Menu{
+		Items: map[BotState]MenuItem{
+			StateMainMenu: MenuItem{
+				Text:    "Main menu",
+				Command: func() { fmt.Println("Вывод главного меню") },
+				Handle:  func() { fmt.Println("Обработчик для меню") },
 			},
-			StatePswdMenu : MenuItem{
-				Text: "pwsd",
-				Command: func() { fmt.Println("Меню с выбором simple и hard") }
-				Handle: func() { fmt.Println("Обработчик для pswd: 
-				Обработка:
-					simple
-					hard
-				") }
+			StatePswdMenu: MenuItem{
+				Text:    "pwsd",
+				Command: func() { fmt.Println("Меню с выбором simple и hard") },
+				Handle: func() {
+					fmt.Println("Обработчик для pswd: Обработка: simple hard")
+				},
 				SubItems: map[BotState]MenuItem{
-					StatePswdMenu1 : MenuItem{
-						Text: "pwsd simple",
-						Command: func() { fmt.Println("генерация simple") }
-						NextMenuId: StateMainMenu
+					StatePswdMenu1: MenuItem{
+						Text:       "pwsd simple",
+						Command:    func() { fmt.Println("генерация simple") },
+						NextMenuId: StateMainMenu,
 					},
-					StatePswdMenu2 : MenuItem{
-						Text: "pwsd hard",
-						Command: func() { fmt.Println("меню hard") }
-						Handle: func() { fmt.Println("Обработчик для настроек hard генерации. 
-						обработка:
-							StatePswdMenu3
-							StatePswdMenu4
-							StatePswdMenu5
-							generate
-						") } 
-						NextMenuId: StateMainMenu
+					StatePswdMenu2: MenuItem{
+						Text:    "pwsd hard",
+						Command: func() { fmt.Println("меню hard") },
+						Handle: func() {
+							fmt.Println("Обработчик для настроек hard генерации.обработка:StatePswdMenu3 StatePswdMenu4 StatePswdMenu5 generate")
+						},
+						NextMenuId: StateMainMenu,
 						SubItems: map[BotState]MenuItem{
-							StatePswdMenu3 : MenuItem{
-								Text: "pwsd настроить количество символов",
-								Command: func() { fmt.Println("настроить количество символов") }
-								Handle: func() { fmt.Println("Обработчик для настройки количества символов
-								Обработка:
-								ввода текста
-								") }
-								NextMenuId: StatePswdMenu2
+							StatePswdMenu3: MenuItem{
+								Text:    "pwsd настроить количество символов",
+								Command: func() { fmt.Println("настроить количество символов") },
+								Handle: func() {
+									fmt.Println("Обработчик для настройки количества символов Обработка: ввода текста")
+								},
+								NextMenuId: StatePswdMenu2,
 							},
-							StatePswdMenu4 : MenuItem{
-								Text: "pwsd настроить количество паролей",
-								Command: func() { fmt.Println("настроить количество паролей") }
-								Handle: func() { fmt.Println("Обработчик для настройки количество паролей 
-								Обработка:
-								ввода текста
-								") }
-								NextMenuId: StatePswdMenu2
+							StatePswdMenu4: MenuItem{
+								Text:    "pwsd настроить количество паролей",
+								Command: func() { fmt.Println("настроить количество паролей") },
+								Handle: func() {
+									fmt.Println("Обработчик для настройки количество паролей Обработка:ввода текста")
+								},
+								NextMenuId: StatePswdMenu2,
 							},
-							StatePswdMenu5 : MenuItem{
-								Text: "pwsd настроить вывод спец символов",
-								Command: func() { fmt.Println("настроить вывод спец символов") }
-								Handle: func() { fmt.Println("Обработчик для настройки вывода спец символов 
-								Обработка:
-								ввода текста
-								") }
-								NextMenuId: StatePswdMenu2
+							StatePswdMenu5: MenuItem{
+								Text:    "pwsd настроить вывод спец символов",
+								Command: func() { fmt.Println("настроить вывод спец символов") },
+								Handle: func() {
+									fmt.Println("Обработчик для настройки вывода спец символов Обработка: ввода текста")
+								},
+								NextMenuId: StatePswdMenu2,
 							},
-						}
+						},
 					},
+				},
+				StateTarrayMenu: MenuItem{
+					Text:    "tarray menu",
+					Command: func() { fmt.Println("Вывод запроса текстка") },
+					Handle: func() {
+						fmt.Println("Обработчик для Для текста")
+					},
+					NextMenuId: StateTarrayMenu1,
+					SubItems: map[BotState]MenuItem{
+						StateTarrayMenu1: {
+							Text:    "меню выбора simple И with quotes",
+							Command: func() { fmt.Println("Вывод запроса текстка") },
+							Handle: func() {
+								fmt.Println("Обработчик для :Для текста")
+							},
+							NextMenuId: StateTarrayMenu,
+						},
+					},
+				},
 			},
-			StateTarrayMenu : MenuItem{
-				Text: "tarray menu",
-				Command: func() { fmt.Println("Вывод запроса текстка") }
-				Handle: func() { fmt.Println("Обработчик для :
-				Для текста
-				") }
-				NextMenuId: StateTarrayMenu1
-				SubItems: map[BotState]MenuItem{
-					StateTarrayMenu1 : {
-						Text: "меню выбора simple И with quotes",
-						Command: func() { fmt.Println("Вывод запроса текстка") }
-						Handle: func() { fmt.Println("Обработчик для :
-						Для текста
-						") }
-						NextMenuId: StateTarrayMenu
-					},
-				}
-			}
-			
-		}
-    }
-	
-	userState := [chatID: StatePswdMenu3]
-    // Добавляем элементы меню
-    menu.AddItem("main", MenuItem{Text: "Main menu",  Command: func() { fmt.Println("") }})
-    menu.AddItem("sub", MenuItem{Text: "Подменю", Command: func() { fmt.Println("Выполняется команда из подменю") }})
+		},
+	}
 
-    // Устанавливаем начальное состояние пользователя
-    menu.SetUserState("user1", "main")
+	userState :=[chatID:StatePswdMenu3]
+// Добавляем элементы меню
+menu.AddItem("main", MenuItem{Text: "Main menu", Command: func () { fmt.Println("") }})
+menu.AddItem("sub", MenuItem{Text: "Подменю", Command: func () { fmt.Println("Выполняется команда из подменю") }})
 
-    // Обрабатываем команду пользователя
-    menu.HandleCommand("user1", "")
+// Устанавливаем начальное состояние пользователя
+menu.SetUserState("user1", "main")
 
-    // Вывод результата в консоль: "Выполняется команда из подменю"
+// Обрабатываем команду пользователя
+menu.HandleCommand("user1", ""),
+
+// Вывод результата в консоль: "Выполняется команда из подменю"
 }
 
 func HandleMenu(state map[int64]BotState, bot *tgbotapi.BotAPI, chatID int64, text string) {
