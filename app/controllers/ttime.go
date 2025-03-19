4b73d17ec7642c2613db33ad218599669ef8a2d4
qunetol <a.belolapotkov@automacon.ru>
2025-03-19 07:41:46 +0300

    add ttime command

--- app/controllers/ttime.go
+++ app/controllers/ttime.go
@@ -31,12 +31,5 @@ func GetData(ts string) string {
 
 	tsTime := ConvertFloatTimestamp(tsFloat)
 	result := "Greenwich Mean Time (UTC+0): " + helpers.Code(tsTime.UTC().Format("02.01.2006 15:04:05")) + "\nEurope/Moscow (UTC+3): " + helpers.Code(tsTime.In(moscowLoc).Format("02.01.2006 15:04:05"))
-	// Вывод различных форматов времени
-	//fmt.Println("Формат \"2006-01-02 15:04:05\":", tsTime.Format("2006-01-02 15:04:05"))
-	//fmt.Println("RFC3339:", tsTime.Format(time.RFC3339))
-	//fmt.Println("RFC1123:", tsTime.Format(time.RFC1123))
-	//fmt.Println("Короткий формат (15:04):", tsTime.Format("15:04"))
-	//fmt.Println("Timestamp (в секундах):", tsTime.Unix())
-	//fmt.Println("Timestamp (в наносекундах):", tsTime.UnixNano())
 	return result
 }
